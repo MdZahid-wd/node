d329c62bb4a393a046acb0999a24dd58506c8d85
MdZahid-wb <zahidmd9830@gmail.com>
2022-10-13 08:16:58 +0530

    kds

--- src/routers/main.js
+++ src/routers/main.js
@@ -227,7 +227,7 @@ routes.post("/student-login",async(req,res)=>{
                  const token=await jwt.sign(user,process.env.JWT_SECRET_KEY,{expiresIn:'1h'});
 
                 
-                res.cookie('jwts',token,{expires:new Date(Date.now()+60*60*1000)});
+                res.cookie('jwts',token,{expires:new Date(Date.now()+60*60*1000),httpOnly:true});
                     
                 res.render("course-videos",{loginLogoName:data.name.split(' ')[0]});
                 console.log("email exists");
